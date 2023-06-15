2048da4d78ae502383625d0d81a09569d6e7d70c
Henok Kirubel <henokhm2@gmail.com>
2023-06-15 18:51:17 +0300

    Add html template and configure html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development', 
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hello Webpack!',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
 };
