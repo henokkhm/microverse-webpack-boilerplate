2c87464007028fbaaa2eadaa5ab9ea0cfb2d419d
Henok Kirubel <henokhm2@gmail.com>
2023-06-15 19:30:04 +0300

    Add Webpack CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Hello Webpack!',
